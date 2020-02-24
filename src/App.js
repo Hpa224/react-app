a9581bb457465f185d6d2414b883d54623664456
Patrick Heron <hpa434@gmail.com>
2020-02-23 22:19:20 -0500

    rendered images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Images from "./components/Images";
+import pictures from "./pictures.json";
+import Wrapper from "./components/Wrapper";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+class App extends Component {
+  state = {
+    pictures
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        {this.state.pictures.map(picture => (
+          <Images
+            id={picture.id}
+            name={picture.name}
+            image={picture.image}
+          />
+        ))}
+        </Wrapper>
+    );
+  }
 }
 
 export default App;
