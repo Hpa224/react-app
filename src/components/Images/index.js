8ae10a9b748fb95fad27a68f0d9a144d0a0baee3
Patrick Heron <hpa434@gmail.com>
2020-02-27 21:37:24 -0500

    fixed allignment and added more pics

--- src/components/Images/index.js
+++ src/components/Images/index.js
@@ -5,7 +5,7 @@ let Image = (props) => {
    return (
       <div className="card">
          <div className="img-container">
-            <img alt={props.name} src={props.image} className="col-4" data-clicked={false} testattr={props.status} id={props.id} onClick={(e) => props.coolFunction(e)}/>
+            <img alt={props.name} src={props.image} className="col" data-clicked={false} testattr={props.status} id={props.id} onClick={(e) => props.coolFunction(e)}/>
          </div>
       </div>
       );
--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./style.css";
+
 
 function Wrapper(props) {
    return <div className="wrapper">{props.children}</div>;
